dfa0ac85166b0def752db671cc193c752d9832ce
ThinhNVo <voti@mail.uc.edu>
2026-01-19 15:42:16 -0500

    Lab1 completed

---
+++ FutureValue/FutureValue/Controllers/AppController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FutureValue.Models;
+
+namespace FutureValue.Controllers
+{
+    public class AppController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Mary";
+            ViewBag.FV = 9999.9;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FV = model.CalculateFutureValue();
+            }
+            else
+            {
+                ViewBag.FV = 0;
+            }
+            return View(model);
+        }
+    }
+}
