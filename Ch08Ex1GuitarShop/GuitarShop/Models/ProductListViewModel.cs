5d8f1f44c28806c485f87aac3d2aa74678924358
ThinhNVo <voti@mail.uc.edu>
2026-03-04 15:01:55 -0500

    Lab 2: Populating our Views with Data

---
+++ Ch08Ex1GuitarShop/GuitarShop/Models/ProductListViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace GuitarShop.Models
+{
+    // Thinh Vo
+    public class ProductListViewModel
+    {
+        public List<Category> Categories { get; set; }
+        public List<Product> Products { get; set; }
+        public string SelectedCategory { get; set; }
+        public string CheckedActiveCategory (string category)
+        {
+            return category == SelectedCategory ? "active" : "";
+        }
+    }
+}
