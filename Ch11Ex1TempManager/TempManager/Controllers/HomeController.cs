1efb632424e38bfebba60959bf7b6ef53e4bc21a
ThinhNVo <voti@mail.uc.edu>
2026-03-30 12:19:37 -0400

    Lab 4 completed

---
+++ Ch11Ex1TempManager/TempManager/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TempManager.Models;
+
+namespace Ch11Ex1TempManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        private TempManagerContext data { get; set; }
+        public HomeController(TempManagerContext ctx) => data = ctx;
+
+        public ViewResult Index()
+        {
+            var temps = data.Temps.OrderBy(t => t.Date).ToList();
+            return View(temps);
+        }
+
+        [HttpGet]
+        public ViewResult Add() => View(new Temp());
+
+        [HttpPost]
+        public IActionResult Add(Temp temp)
+        {
+            if (temp.Date.HasValue && data.Temps.Any(t => t.Date == temp.Date)) {
+                ModelState.AddModelError("Date", "A record with that date already exists.");
+            }
+
+            if (ModelState.IsValid) {
+                data.Temps.Add(temp);
+                data.SaveChanges();
+                return RedirectToAction("Index");
+            } 
+            else {
+                ModelState.AddModelError("", "Please correct all errors and try again.");
+                return View(temp);
+            }
+        }
+
+        [HttpGet]
+        public ViewResult Delete(int id)
+        {
+            var temp = data.Temps.Find(id);
+            return View(temp);
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Delete(Temp temp)
+        {
+            data.Remove(temp);
+            data.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Ch11Ex1TempManager/TempManager/Controllers/ValidationController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using TempManager.Models;
+namespace TempManager.Controllers
+{ 
+    public class ValidationController : Controller
+    {
+        private TempManagerContext data { get; set; }
+        public ValidationController(TempManagerContext context) { 
+            data = context;
+        }
+
+        public JsonResult CheckDate(DateTime? date)
+        {
+            if (date == null) {
+                return Json("Please enter a date.");
+            }
+
+            string msg = data.Temps.FirstOrDefault(t => t.Date == date) == null ? "" : "A record with that date already exists.";
+            if (string.IsNullOrEmpty(msg)) {
+                return Json(true);
+            } else {
+                return Json(msg);
+            }
+        }
+    }
+}
